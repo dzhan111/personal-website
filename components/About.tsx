6156f0d04ff164b88836b7ddb1c7b315a4126a04
dzhan111 <zhandavid4@gmail.com>
2024-11-12 16:20:38 -0500

    new resume

--- components/About.tsx
+++ components/About.tsx
@@ -10,12 +10,12 @@ const About = () => {
     return (
         <div id="about" className="flex flex-col items-center justify-center h-screen bg-background text-foreground p-6">
             <div className="max-w-xl text-left space-y-4 mx-40">
-                <h1 className="text-5xl font-bold">Hi, I'm David.</h1>
+                <h1 className="text-5xl font-bold">Hi, I&apos;m David.</h1>
                 <p className="text-lg py-10">
                     I am a junior at the University of Pennsylvania pursuing a Bachelor&apos;s in Computer Science with Minors in Math and Data Science.
                     <br />
                     <br />
-                    I’m passionate about building solutions that are both efficient and user-centered. In the summer of 2024, I worked as a Software Engineering Intern for a UMD start-up called Ventoscity. I redesigned their entire mobile app using Flutter, incorporating a GPT-4-powered food analysis feature.
+                    I&apos;m passionate about building solutions that are both efficient and user-centered. In the summer of 2024, I worked as a Software Engineering Intern for a UMD start-up called Ventoscity. I redesigned their entire mobile app using Flutter, incorporating a GPT-4-powered food analysis feature.
                     <br />
                     <br />
                     During my time at school, I also work part-time as a Frontend Software Engineer for keep.id, a startup with the goal of providing proper identification for the homeless.

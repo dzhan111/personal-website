523c4a7cf93dcc5ee10e137afe383be9a916ebd6
dzhan111 <zhandavid4@gmail.com>
2024-12-20 02:20:43 -0500

    navbar clean up

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,32 +7,39 @@ const Navbar = () => {
         <nav className="bg-primary-foreground text-primary py-4 shadow-md">
             <div className="container mx-auto flex justify-between items-center px-6">
                 {/* Logo or Brand */}
-                <div className="text-xl font-bold">
+                <div className="text-xl font-bold justify-start">
                     <Link href="/">
                         David Zhan
                     </Link>
                 </div>
-
-                {/* Navigation Links */}
-                <ul className="flex space-x-6">
-                    <li>
-                        <Link href="/" className="hover:text-secondary transition">
-                            About
-                        </Link>
-                    </li>
-                    <li>
-                        <Link href="/projects" className="hover:text-secondary transition">
-                            Projects
-                        </Link>
-                    </li>
-                   
-                </ul>
-
-                {/* Theme Toggle Button */}
-                <div>
-                    {/* Import your Toggle component here */}
-                    <Toggle />
+                <div className='flex justify-center'>
+                    {/* Navigation Links */}
+                    <ul className="flex space-x-8 py-2">
+                        <li>
+                            <Link href="/" className="hover:text-secondary transition">
+                                About
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/projects" className="hover:text-secondary transition">
+                                Projects
+                            </Link>
+                        </li>
+                        <li>
+                            <Link href="/projects" className="hover:text-secondary transition">
+                                Other
+                            </Link>
+                        </li>
+                    
+                    </ul>
+                    {/* Theme Toggle Button */}
+                    <div className='px-4'>
+                        {/* Import your Toggle component here */}
+                        <Toggle />
+                    </div>
                 </div>
+
+                
             </div>
         </nav>
     );

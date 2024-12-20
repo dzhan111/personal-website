c050c3c89806e625b63b241711c28c207b269511
dzhan111 <zhandavid4@gmail.com>
2024-12-20 02:24:52 -0500

    vercel pls

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider attribute="class" defaultTheme="light">
+        <ThemeProvider attribute="class" defaultTheme="system">
           <Navbar />
           <main>{children}</main>
         </ThemeProvider>
